29ed3f27c0a192d4b99bb94ff9a7f987486b7892
eden.kim <eden.kim@goorm.io>
2023-09-21 17:11:31 +0900

    feat: 이미지 파일의 path 설정을 next.config 에서 수행하도록 수정

--- krampoline/next.config.js
+++ krampoline/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.BASE_PATH,
+    images: {
+        loader: 'imgix',
+        path: process.env.BASE_PATH,
+    },
     output: 'standalone',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
