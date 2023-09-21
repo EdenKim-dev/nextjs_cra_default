e95577617a8bbb50f06c45b677df4e6e20054f27
eden.kim <eden.kim@goorm.io>
2023-09-21 17:54:00 +0900

    fix: image 관련 설정 원복

--- krampoline/next.config.js
+++ krampoline/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.BASE_PATH,
-    images: {
-        loader: 'imgix',
-        path: process.env.BASE_PATH,
-    },
     output: 'standalone',
 };
 
